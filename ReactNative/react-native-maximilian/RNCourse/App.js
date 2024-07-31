ad15a0f49dfe36e530ed8670df534db9e15ee9f3
Gabriel Viana <gabrielguilherme13@hotmail.com>
2024-07-31 09:47:19 -0300

    feat: my first react native app on udemy maximilian course

---
+++ ReactNative/react-native-maximilian/RNCourse/App.js
@@ -0,0 +1,30 @@
+import { Alert, StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  // Alert.alert('Teste', 'Teste de Alert')
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.dummyText}>Another piece of text</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World</Text>
+      <Button title="Tap me!" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "red",
+    padding: 16,
+  },
+});
